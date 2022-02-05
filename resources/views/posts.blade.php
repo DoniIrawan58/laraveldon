1e0eabc84c4d7b86b3c33ae6c2d9159c915f303f
Doni_Irawan <donicyber5@gmail.com>
2022-02-05 14:57:28 +0700

    zzzzzz

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,11 +5,14 @@
 
 
 @foreach ($posts as $post)
-<article>
-<h2>
- <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-</h2>
-<p>{{ $post->body }}</p>
+<article class="mb5 border-bottom">
+<h2><a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h2>
+
+<p>By. Doni Irawan in <a href="/categories/{{ $post->category->slug }}"class="text-decoration-none">{{
+    $post->category->name }}</a></p>
+<p>{{ $post->excerpt }}</p>
+
+<a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
 </article>
 @endforeach
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -34,4 +35,19 @@ Route::get('/about', function () {
 
 
 Route::get('/posts', [PostController::class, 'index']);
-Route::get('posts/{slug}', [PostController::class, 'show']);
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function(){
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+   ]);
+});
+
+Route::get('/categories/{category:slug}', function(Category $category){
+    return view('category', [
+         'title' => $category->name,
+         'posts' => $category->posts,
+         'category' => $category->name
+    ]);
+});
